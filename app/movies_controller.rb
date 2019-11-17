c2885cd115b987318ef48139e0e610d6a89f6d8b
Blythe Pollard <blytheepollard@gmail.com>
2019-11-17 04:38:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,9 +25,10 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = { title: "Home alone", release date: 1990})
+def can_be_created_in_a_block(args = { title: "Home alone", release_date: 1990})
   Movie.create do |m|
-    __
+    m.string :title 
+    m.integer :release_date
   end
 end
 
